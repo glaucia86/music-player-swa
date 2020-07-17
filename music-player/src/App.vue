96f03da39e3ca190419423e3849ca27a4618a709
glaucia86 <glaucia_lemos86@hotmail.com>
2020-07-17 00:11:08 -0300

    Initial commit with Vue.js

---
+++ music-player/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <MusicPlayer msg="Welcome to the SWA + GitHub Actions + Vue.js Application"/>
+  </div>
+</template>
+
+<script>
+import MusicPlayer from './components/MusicPlayer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MusicPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ music-player/src/components/MusicPlayer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="musicplayer">
+    <h1>Music player with P5.js</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MusicPlayer',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
